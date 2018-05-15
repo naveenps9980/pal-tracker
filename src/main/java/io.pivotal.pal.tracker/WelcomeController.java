a8dc9fb478034705ed5938a4fc40916688953eed
Pivotal <pivotal@Pivotals-iMac-9.local>
2018-05-15 05:51:55 -0700

    Implemented code for testcases

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -1,13 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String welcomeMessage;
+
+    @Autowired
+    public WelcomeController(
+            @Value("${WELCOME_MESSAGE}") String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeMessage;
     }
 }
